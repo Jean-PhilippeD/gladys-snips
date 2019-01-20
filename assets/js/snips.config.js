5ac977cd16c209b9311a7f0817e2c6308429c2e1
Jean-PhilippeD <jp.danree@free.fr>
2019-01-20 21:53:26 +0100

    Adding description in view

--- assets/js/snips.config.js
+++ assets/js/snips.config.js
@@ -11,7 +11,11 @@ var translationsEN = {
     WAKEWORD_TOOLTIP: 'Wakewords can be generic, or specific for each user, this way, snips can recognize users',
     CONFIGURE_MQTT_URL: 'MQTT Broker address',
     DEFAULT_MQTT_URL: 'mqtt://localhost:1883',
-    MANAGE_SLOTS: 'Manage known slots values'
+    MANAGE_SLOTS: 'Manage known slots values',
+    DESC_MQTT_URL: '',
+    DESC_SATELLITE: '',
+    DESC_WAKEWORDS: '',
+    DESC_SLOTS: ''
 }
 
 var translationsFR = {
@@ -27,7 +31,11 @@ var translationsFR = {
     WAKEWORD_TOOLTIP: 'Les mots de déclenchements peuvent être propre à chaque utilisateur, permettant ainsi la reconnaissance d\'utilisateur',
     CONFIGURE_MQTT_URL: 'Addresse du broker MQTT',
     DEFAULT_MQTT_URL: 'mqtt://localhost:1883',
-    MANAGE_SLOTS: 'Gérer les données connues par entités'
+    MANAGE_SLOTS: 'Gérer les données connues par entités',
+    DESC_MQTT_URL: 'Il s\'agit de l\'adresse du broker MQTT qu\'utilise Snips pour communiquer.',
+    DESC_SATELLITE: 'Vous pouvez désactiver le feedback (il s\'agit du son produit par Snips pour notifier le début d\'écoute et la fin d\'écoute) pour chaque satellite. Attention, cela reste temporaire, pour le faire définitivement, veuillez mettre la valeur sound_feedback_enabled_default à false dans le fichier de configuration /etc/snips.toml',
+    DESC_WAKEWORDS: 'Vous pouvez définir pour chaque utilisateur, le wakeword personnalisé que vous avez défini dans snips, ceci afin de faire de la reconnaissance de personne. Par défaut, c\'est le premier utilisateur qui est choisi.',
+    DESC_SLOTS: 'Chaque slot correspond à un emplacement dans une phrase qui peut être rempli par les valeurs associées, ceci afin d\'améliorer la taille du dictionnaire local.'
 }
 
 angular
